837235248e008d9fa488a1ad8ddf0656503da7d9
MrBeanB <shannonc1620@gmail.com>
2024-08-05 11:58:23 -0400

    state filter
    available and unavailable state of the pets

--- src/js/filter.js
+++ src/js/filter.js
@@ -1,20 +1,22 @@
 const Filter = {
   init: function () {
     // Attach change event listeners to filters
-    $('#breedFilter, #ageFilter, #locationFilter').change(Filter.applyFilters);
+    $('#breedFilter, #ageFilter, #locationFilter, #stateFilter').change(Filter.applyFilters);
   },
 
   applyFilters: function () {
     const breedFilter = $('#breedFilter').val();
     const ageFilter = $('#ageFilter').val();
     const locationFilter = $('#locationFilter').val();
+    const stateFilter = $('#stateFilter').val();
 
     // Filter pets based on selected criteria
     const filteredPets = App.pets.filter((pet) => {
       const breedMatch = breedFilter === '' || pet.breed === breedFilter;
       const ageMatch = ageFilter === '' || pet.age == ageFilter;
       const locationMatch = locationFilter === '' || pet.location === locationFilter;
-      return breedMatch && ageMatch && locationMatch;
+      const stateMatch = stateFilter === '' || pet.state === stateFilter;
+      return breedMatch && ageMatch && locationMatch && stateMatch;
     });
 
     // Display filtered pets
